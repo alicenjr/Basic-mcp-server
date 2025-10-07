14ed7b0ff2a69425aa81f0cdfd39d5a75bf93fbf
Niraj hosad <hniraj51@gmail.com>
2025-10-07 10:56:13 +0530

    understanding basic mcp server

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+from fastmcp import FastMCP
+
+mcp = FastMCP(name="expense-tracker-mcp-server")
+
+@mcp.tool
+def roll_dice(n_dice:int=1)->list[int]:
+    """roll n_dice 6 sided dice and return the results"""
+    return [random.randint(1, 6) for _ in range(n_dice)]
+@mcp.tool
+def add_numbers(a:float,b:float)->float:
+    """add two numbers together"""
+    return a+b
+
+if __name__ == "__main__":
+    mcp.run()
